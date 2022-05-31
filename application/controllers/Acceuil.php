adb1273bc78950c7d3b26350d457546e0f4fd5bb
Josaphat <josaphatkalonji99@gmail.com>
2022-05-31 22:46:07 +0200

    start project

---
+++ application/controllers/Acceuil.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Acceuil extends CI_Controller {
+
+	
+	public function index()
+	{
+		$this->load->view('pages/acceuil');
+	}
+}
