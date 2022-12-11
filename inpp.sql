4cd406f50cb1152cda6d8e059c19d34171a02391
Josaphat <josaphatkalonji99@gmail.com>
2022-12-11 19:51:00 +0200

    adding db file

--- inpp.sql
+++ inpp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 08, 2022 at 08:35 PM
+-- Generation Time: Dec 11, 2022 at 06:49 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.4
 
@@ -61,13 +61,9 @@ CREATE TABLE `compte` (
 --
 
 INSERT INTO `compte` (`id`, `username`, `password`, `type_compte`, `utilisateur_id`) VALUES
-(1, 'youri', '0000', 'candidat', 1),
-(2, 'eliezer', '0000', 'candidat', 2),
-(3, 'dan', '0000', 'candidat', 3),
-(4, 'emma', '0000', 'finance', 4),
 (5, 'nambe', '0000', 'admin', 5),
-(6, '@Jemimah', '@Jemimah2022', 'finance', 6),
-(7, '@junior', '@junior2022', 'finance', 7);
+(7, '@junior', '@junior2022', 'finance', 7),
+(10, '@bwana', '@bwana2022', 'candidat', 10);
 
 -- --------------------------------------------------------
 
@@ -90,9 +86,8 @@ CREATE TABLE `detail_formation` (
 
 INSERT INTO `detail_formation` (`id`, `date_debut`, `date_fin`, `etat`, `formation_id`, `utilisateur_id`) VALUES
 (1, NULL, NULL, 'ongoing', 2, 3),
-(2, NULL, NULL, 'ongoing', 1, 2),
-(3, NULL, NULL, 'ongoing', 3, 2),
-(4, NULL, NULL, 'ongoing', 2, 2);
+(2, NULL, NULL, 'ongoing', 2, 10),
+(3, NULL, NULL, 'finished', 1, 10);
 
 -- --------------------------------------------------------
 
@@ -112,10 +107,10 @@ CREATE TABLE `document` (
 --
 
 INSERT INTO `document` (`id`, `nom_fichier`, `type`, `utilisateur_id`) VALUES
-(1, 'fichier5dcf6a1dcb2d004910fca3c5017b5f41_ProgrammingSteps.png', 'lettre', 2),
-(2, 'fichier5dcf6a1dcb2d004910fca3c5017b5f41_1200px-MVC-Process.svg.png', 'identite', 2),
 (3, 'fichierfbfa67aef81be6cdf8db08693c87b808_OneFamily_(1).png', 'lettre', 3),
-(4, 'fichierfbfa67aef81be6cdf8db08693c87b808_OneFamily_(1).jpg', 'identite', 3);
+(4, 'fichierfbfa67aef81be6cdf8db08693c87b808_OneFamily_(1).jpg', 'identite', 3),
+(7, 'fichier51502c9784d3e4807222fd42743c34f4_Canevas_à_suivre_pour_le_TFC_2022-1.pdf', 'lettre', 10),
+(8, 'fichier51502c9784d3e4807222fd42743c34f4_profile.jpeg', 'identite', 10);
 
 -- --------------------------------------------------------
 
@@ -138,7 +133,8 @@ CREATE TABLE `formation` (
 INSERT INTO `formation` (`id`, `intitule`, `duree`, `tarif`, `branche_id`) VALUES
 (1, 'Electronique', '6 mois', 70, 1),
 (2, 'Musique', '4 mois', 50, 3),
-(3, 'MS Excel', '3 mois', 60, 2);
+(3, 'MS Excel', '3 mois', 60, 2),
+(4, 'Mecanique', '9 mois', 200, 1);
 
 -- --------------------------------------------------------
 
@@ -160,9 +156,8 @@ CREATE TABLE `paiement` (
 
 INSERT INTO `paiement` (`id`, `date`, `montant`, `utilisateur_id`, `formation_id`) VALUES
 (2, '28-11-2022', '50', 3, 2),
-(3, '08-12-2022', '70', 2, 1),
-(4, '08-12-2022', '60', 2, 3),
-(5, '08-12-2022', '46', 2, 2);
+(3, '11-12-2022', '50', 10, 2),
+(4, '11-12-2022', '70', 10, 1);
 
 -- --------------------------------------------------------
 
@@ -179,21 +174,19 @@ CREATE TABLE `utilisateur` (
   `genre` varchar(8) DEFAULT NULL,
   `matricule` varchar(45) DEFAULT NULL,
   `type_candidat` varchar(45) DEFAULT NULL,
-  `poste` varchar(45) DEFAULT NULL
+  `poste` varchar(45) DEFAULT NULL,
+  `created_at` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `utilisateur`
 --
 
-INSERT INTO `utilisateur` (`id`, `nom_complet`, `email`, `phone`, `adresse`, `genre`, `matricule`, `type_candidat`, `poste`) VALUES
-(1, 'KATOMB MUKAZ YOURI', '16kk112@esisalama.org', '+260772630674', 'Lubumbashi', 'Male', NULL, 'personel', NULL),
-(2, 'BWANA WA BWANA ELIEZER', 'kazembe@gmail.com', '+260772630674', 'kansenshi', 'Male', NULL, 'eleve', NULL),
-(3, 'Kyungu Daniel', 'kazembe@gmail.com', '+260 772630674', 'kansenshi', 'Male', NULL, 'personel', NULL),
-(4, 'Emmauel Musongo', 'emma@gmail.com', '+260 777500200', 'Lubumbashi', 'male', '1234em22', NULL, 'Finance'),
-(5, 'Bianca Nambe', 'nambe@gmail.com', '+260 971944299', 'Lubumbashi', 'female', '1234em20', NULL, 'admin'),
-(6, 'Jemimah Kalonje', 'surmpy.engineering@yahoo.com', '+260 765 721 319', 'Lubumbashi', 'female', '22JK426', NULL, 'Financier'),
-(7, 'Junior Kasenda', 'kazembe@gmail.com', '+260 971944299', 'kansenshi', 'male', '22JK133', NULL, 'Financier');
+INSERT INTO `utilisateur` (`id`, `nom_complet`, `email`, `phone`, `adresse`, `genre`, `matricule`, `type_candidat`, `poste`, `created_at`) VALUES
+(3, 'Kyungu Daniel', 'kazembe@gmail.com', '+260 772630674', 'kansenshi', 'male', NULL, 'personel', NULL, NULL),
+(5, 'Bianca Nambe', 'nambe@gmail.com', '+260 971944299', 'Lubumbashi', 'female', '1234em20', NULL, 'admin', NULL),
+(7, 'Junior Kasenda', 'kazembe@gmail.com', '+260 971944299', 'kansenshi', 'male', '22JK133', NULL, 'Financier', NULL),
+(10, 'BWANA WA BWANA ELIEZER', '16kk112@esisalama.org', '+260772630674', 'Lubumbashi', 'male', NULL, 'personel', NULL, '10-12-2022');
 
 --
 -- Indexes for dumped tables
@@ -262,37 +255,37 @@ ALTER TABLE `branche`
 -- AUTO_INCREMENT for table `compte`
 --
 ALTER TABLE `compte`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `detail_formation`
 --
 ALTER TABLE `detail_formation`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `document`
 --
 ALTER TABLE `document`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `formation`
 --
 ALTER TABLE `formation`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `paiement`
 --
 ALTER TABLE `paiement`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Constraints for dumped tables
